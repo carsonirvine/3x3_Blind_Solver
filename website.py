60939c58a5801dba4b49d9cbfd3a80cf7bfe0af9
carsonirvine <carsonspencerirvine@gmail.com>
2026-01-12 21:22:02 -0800

    Revert "Added ability to use ngrok to host locally but access publicly"
    This reverts commit e8f8f75338028754a9d5c1664cb60a8c03338e2a.

--- website.py
+++ website.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 from Solvers.blind_solver import Blind_Solver
-import ngrok
-ngrok.set_auth_token("38BkixqHiNdYVKDre2eJrQUncvi_4SuizwB7Y3u9iQ4YtqXMX")
 
 app = Flask(__name__)
 
@@ -29,10 +27,4 @@ def solve():
     return jsonify({"result": output})
 
 if __name__ == "__main__":
-
-    port = 5000
-    #ngrok.kill()
-    #public_url = ngrok.connect(port)
-    #print(" * ngrok tunnle URL:", public_url)
-    
-    app.run(port = port, debug=True)
+    app.run(debug=True)
